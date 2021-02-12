3191bb45d7f9668dfd33b14e755abfbcc175f32e
Vinicius Gabriel <vinicius.gabriel@webjump.com.br>
2021-02-12 10:44:19 -0300

    Criando TransactionModel com metodo getInfo

---
+++ app/model/TransactionModel.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_errors', 'on');
+
+class TransactionModel
+{
+
+    public function __construct()
+    {
+        $this->connection = new PDO("mysql:host=localhost;port=3306;dbname=wjcrypto", "vinicius", "webjump");
+    }
+
+    public function getInfo($id)
+    {
+        $stmt = $this->connection->prepare("SELECT * FROM transaction_type WHERE id = :id;");
+        $stmt->execute([":id" => $id]);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $value = $result["0"];
+        $description = $value["description"];
+        return $description;
+
+    }
+}
+
+$test = new TransactionModel();
+$result = $test->getInfo(1);
+var_dump($result);
+
+?>
